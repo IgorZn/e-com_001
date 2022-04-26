7a5e95596616e3e94b70ff7e4e0f79d8624319a1
Igor Znamensky <igor.znamensky@gmail.com>
2022-04-26 10:08:48 +0300

    06 07. Testing Image Upload with Postman (fixed)

--- utils/upload.utils.js
+++ utils/upload.utils.js
@@ -1,11 +1,15 @@
 const multer = require("multer");
 const path = require("path");
 
-const uploadPath = process.env.UPLOAD_PATH;
+// Get .env
+require('dotenv/config')
+
+const uploadPath = '.' + process.env.UPLOAD_PATH;
+console.log('UPLOAD_PATH -->>', path.resolve(uploadPath))
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, `.${uploadPath}`)
+    cb(null, path.resolve(uploadPath))
   },
   filename: function (req, file, cb) {
     cb(null, file.originalname.split('.')[0]  + '_' + Date.now() + path.extname(file.originalname))
